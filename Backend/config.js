2a43376a0a1b9df9385e7d12a8021a4e7ddb09d2
Alejandro1412 <alejopola14@hotmail.com>
2020-05-30 01:35:47 -0500

    Cree middlewares, refine controllers, login funcional

---
+++ Backend/config.js
@@ -0,0 +1,39 @@
+//---------------------------------
+// ARCHIVO DE CONFIGURACIÓN GLOBAL
+//--------------------------------
+
+
+//----------- PUERTO 3000----//
+process.env.PORT = process.env.PORT || 3000;
+
+
+//--------------------------//
+// VENCIMIENTO TOKEN
+//--------------------------//
+process.env.CADUCIDAD_TOKEN=60*60*24*30;
+
+//--------------------------
+// ENTORNOS Base de datos, Desarrollo o producción
+//--------------------------
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//--------------------------//
+// VENCIMIENTO TOKEN
+//--------------------------//
+process.env.SEED=process.env.SEED || 'seed-de-desarrollo';
+
+
+//--------------------------//
+// Bases de datos
+//--------------------------//
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/fgmc';
+} else {
+    urlDB = process.env.MONGO_URI;
+}
+
+process.env.URLDB = urlDB;
--- Backend/models/usuario.model.js
+++ Backend/models/usuario.model.js
@@ -1,26 +1,36 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
-let contratosSchema = new mongoose.Schema({
-    nombreContrato: { type: String, required: [true, 'El nombre del contrato es requerido'] },
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuarios', required: [true, 'UN USUARIO ID ES REQUERIDO'] },
-    lugarExpedicion: { type: String, required: [true, '']},
-    fechaNacimiento: { type: Date, required: [true, '']},
-    CajaCompensacion: { type: String, required: [true, '']},
-    afp_FondoPensiones: { type: String, required: [true, ''] },
-    eps: { type: String, required: [true, ''] },
-    fechaIngreso: { type: Date, required: [true, ''] },
-    fechaRetiro: { type: Date, required: [true, '']},
-    salario: { type: Number, required: [true, '']},
-    auxilioporTrayecto: { type: Number},
-    auxilioporTransporte: { type: Number},
-    cargo: { type: String },
-    titulo: { type: String, required: [true, '']},
-    universidad: { type: String, required: [true, ''] },
-    telefono: { type: Number, required: [true, ''] },
-    direccion: { type: String, required: [true, '']},
-    email: {type: String, required: [true, ''] },
-    observacion: { type: String },
+let rolesValidos = {
+    values: ['adminRol','ingeniero','talentoHumano','auxSena'],
+    message: '{VALUE} NO ES UN ROL VALIDO'
+}
+
+let usuarioSchema = new mongoose.Schema({
+    nombre: { type: String, required: [true, 'Un nombre es requerido'] },
+    apellido: { type: String, required: [true, 'Un Apellido es requerido'] },
+    rol: {  type: String, default: 'auxSena', enum: rolesValidos },
+    email: { type: String, unique: true, required: [true, 'Un E-mail es requerido'] },
+    password: { type: String, required: [true, 'Su password es requerida'] },
+    activeUser: { type: Boolean, default: true },
     date: { type: Date, default: Date.now }
 });
+usuarioSchema.methods.encryptPassword = async(password) => {
+    let salt = await bcrypt.genSalt(saltRounds);
+    let hash = bcrypt.hash(password, salt);
+    return hash;
+}
+//BORRO LA CONRASEÑA DE LOS RESPONSE
+usuarioSchema.methods.toJSON = function() {
+    let user = this;
+    let userObject = user.toObject();
+    delete userObject.password;
 
-module.exports = mongoose.model('contrato', contratosSchema);
+    return userObject;
+}
+usuarioSchema.plugin(uniqueValidator, { message: '{PATH} DEBE SER UNICO' })
+module.exports = mongoose.model('usuario', usuarioSchema);
+
+

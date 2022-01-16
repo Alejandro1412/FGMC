4b472226268ea8b6bf2f5f0150b7ef39c10c6dd8
AlejandroPolancoA <alejopola14@hotmail.com>
2022-01-16 12:23:31 -0500

    Test: Get contracts with heroku

--- Backend/routes/contratos.routes.js
+++ Backend/routes/contratos.routes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const contratosCtrl = require('../controllers/contratos.controller');
 const {verificaToken,verificaAdmin_Rol} = require('../middlewares/autenticacion');
 
-router.get('/contratos',[verificaToken], contratosCtrl.getContratos);
+router.get('/contratos', contratosCtrl.getContratos);
 router.post('/contrato',[verificaToken,verificaAdmin_Rol], contratosCtrl.createContrato);
 router.get('/contrato/:id',[verificaToken,verificaAdmin_Rol], contratosCtrl.getContrato);
 router.put('/contrato/:id',[verificaToken,verificaAdmin_Rol],contratosCtrl.editContrato);

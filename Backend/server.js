6a2f4cb34cec782ba85328bbec97dbd527de00dc
AlejandroPolancoA <alejopola14@hotmail.com>
2022-03-20 12:18:27 -0500

    Fix ports 3000 and 3001 final

--- Backend/server.js
+++ Backend/server.js
@@ -18,7 +18,7 @@ app.use(morgan('dev'));
 app.use(express.json());//METODO QUE HACE QUE EL CODIGO DEL NAVEGADOR ENTIENDA JSONS
 app.use(express.urlencoded({ extended: false })); //POSTMAN PRUEBA EN CAJAS
 app.use(cors({ origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
+    if (whiteList.indexOf(origin) !== -1) {
       callback(null, true)
     } else {
       callback(new Error('Not allowed by CORS'))
